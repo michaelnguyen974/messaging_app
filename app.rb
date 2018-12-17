f8dd7c8772b6211c2846112fe8af5370ea9dec41
Michael Nguyen <michaelnguyen974@gmail.com>
2018-12-17 16:00:47 +0000

    created messages path, history erb to store msg history, added post feature

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ class Messages < Sinatra::Base
   get '/' do
     erb :index
   end
-
+  
+  post '/messages' do
+    @message1 = params[:input_message]
+    erb(:history)
+  end
 run! if app_file == $0
 end
---
+++ spec/features/messages_spec.rb
@@ -0,0 +1,11 @@
+require 'capybara/rspec'
+require './app.rb'
+
+feature "" do 
+  scenario "" do 
+    visit('/')
+    fill_in 'input_message', with: "Michael"
+    click_button "Submit"
+    expect(page).to have_text("Michael")
+  end 
+end 
